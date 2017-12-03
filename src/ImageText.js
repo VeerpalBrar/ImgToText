05664dca359b45aa4b6960a20a40b5827f6cead8
VeerpalBrar <v.brar@mail.utoronto.ca>
2017-12-03 10:24:06 -0500

    fetch api result

--- src/ImageText.js
+++ src/ImageText.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 class ImageText extends Component {
     constructor(props) {
       super(props);
-      this.state = {url: ''};
+      this.state = {
+          url: '', 
+          text: ''};
   
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
@@ -15,17 +17,53 @@ class ImageText extends Component {
     handleSubmit(event) {
       alert('A name was submitted: ' + this.state.url);
       event.preventDefault();
+      fetch('https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/ocr?', {
+        method: 'post',
+        headers: new Headers({
+        'Content-Type': 'application/json', 
+        'Ocp-Apim-Subscription-Key':'keygoeshere',
+	}), 
+        body: JSON.stringify({
+           url: this.state.url
+        })
+    }).then(res => res.json())
+    .then(
+      (result) => {
+        console.log(JSON.stringify(result));
+        if(result.code){
+            this.setState({text: "Something went wrong! \n" + result.code + " : " + result.message});
+        } else{
+            var imgwords =""
+            result.regions.forEach(function(region) {
+                region.lines.forEach(function(line) {
+                    line.words.forEach(function(word){
+                        imgwords+= word.text + " "
+                    })
+                })
+            });
+            console.log(imgwords);
+            this.setState({text: imgwords});
+        }
+        }, 
+      
+      (error) => {
+        console.log(error);
+      }
+    )
     }
   
     render() {
       return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Image URL:
-            <input style={{marginLeft: 1 + 'em'}}type="text" value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <input style={{marginLeft: 1 + 'em'}} type="submit" value="Submit" />
-        </form>
+        <div>
+            <form onSubmit={this.handleSubmit}>
+            <label>
+                Image URL:
+                <input style={{marginLeft: 1 + 'em'}}type="text" value={this.state.value} onChange={this.handleChange} />
+            </label>
+            <input style={{marginLeft: 1 + 'em'}} type="submit" value="Submit" />
+            </form>
+            <p>{this.state.text}</p>
+        </div>
       );
     }
   }
